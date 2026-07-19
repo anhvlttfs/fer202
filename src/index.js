945e48d9927256c2a6dfb133284bcacf4a84a2fc
Vo Luu Tuong Anh <anhvlt.2006@outlook.com>
2026-07-19 13:58:43 +0700

    add login behaves (#14)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Context from './context';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Context>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Context>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
