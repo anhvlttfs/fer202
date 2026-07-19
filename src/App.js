cc9ed89a3fcbc17b44cc09687d8d04bdd25a033a
Trương Vũ Bình <Vubinh.20050309@gmail.com>
2026-07-19 12:46:47 +0700

    binhtv-admin-phase1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import { TopBar, Footer } from "./components/index"
+import { TopBar, Footer } from "./components/index";
 import { Home, Login, Book, Reader } from "./pages";
-
+import Admin from "./pages/Admin";
 const App = () => {
-    return (
-        <>
-            <header>
-                <TopBar />
-            </header>
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/book" element={<Book />} />
-                    <Route path="/book/:id" element={<Reader />} />
-                </Routes>
-            </main>
-            <footer>
-                <Footer />
-            </footer>
-        </>
-    );
-}
+  return (
+    <>
+      <header>
+        <TopBar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/book/:id" element={<Reader />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
 
 export default App;
---
+++ src/pages/Admin/index.js
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+import BookForm from "../../components/BookForm";
+
+export default function Admin() {
+  const [books, setBooks] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [editingBook, setEditingBook] = useState(null);
+
+  const loadBooks = () => {
+    fetch("http://localhost:3000/books")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        console.log(Array.isArray(data)); // phải là true
+        setBooks(data);
+      });
+  };
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  // ✅ ADD
+  const handleAdd = (data) => {
+    const numericIds = books
+      .map((book) => Number(book.id))
+      .filter((id) => !isNaN(id));
+
+    const maxId = Math.max(...numericIds, 0);
+
+    const newBook = {
+      id: String(maxId + 1),
+      ...data,
+    };
+
+    fetch("http://localhost:3000/books", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBook),
+    }).then(() => {
+      setShowForm(false);
+      loadBooks();
+    });
+  };
+  // ✅ EDIT
+  const handleEdit = (data) => {
+    fetch(`http://localhost:3000/books/${editingBook.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        ...editingBook,
+        ...data,
+      }),
+    }).then(() => {
+      setEditingBook(null);
+      setShowForm(false);
+      loadBooks();
+    });
+  };
+
+  // DELETE
+  const deleteBook = (id) => {
+    fetch(`http://localhost:3000/books/${id}`, {
+      method: "DELETE",
+    }).then(loadBooks);
+  };
+
+  // CLICK EDIT
+  const editBook = (book) => {
+    setEditingBook(book);
+    setShowForm(true);
+  };
+
+  return (
+    <div className="admin-container">
+      <h2>ADMIN - MANAGE BOOKS</h2>
+
+      <button
+        onClick={() => {
+          setEditingBook(null);
+          setShowForm(true);
+        }}
+      >
+        Add Book
+      </button>
+
+      <table className="admin-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Genre</th>
+            <th>Year</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {books.map((b) => (
+            <tr key={b.id}>
+              <td>{b.id}</td>
+              <td>{b.title}</td>
+              <td>{b.author}</td>
+              <td>{b.genre}</td>
+              <td>{b.year}</td>
+              <td>
+                <button onClick={() => editBook(b)}>Edit</button>
+                <button onClick={() => deleteBook(b.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {showForm && (
+        <BookForm
+          onSubmit={editingBook ? handleEdit : handleAdd}
+          editingBook={editingBook}
+          onCancel={() => setShowForm(false)}
+        />
+      )}
+    </div>
+  );
+}
